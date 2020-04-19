f494733e61e4728fc0faa3aa50a8af9f7fbf9bd8
pinnamanivenkat <pinnamanivenkat@gmail.com>
2020-04-19 11:00:21 +0530

    ESLint: Fix unused variables

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
-import express from 'express'
-import homeRouter from './home-router'
+import express from 'express';
+import homeRouter from './home-router';
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/', homeRouter)
+router.use('/', homeRouter);
 
-router.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
-})
+router.use((_req, res, _next) => {
+  res.status(404).send("Sorry can't find that!");
+});
 
-export default router
+export default router;
