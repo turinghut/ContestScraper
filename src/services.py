bf5e0eff42809c612c56faaa55821f096aa32757
Abhishek <abhishekthalatoty@gmail.com>
2020-05-09 11:38:27 +0530

    ContestsRetreiver: added getContestDetails method, added duration and end_time

--- src/services.py
+++ src/services.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil import parser
 from .models import Contest
 from PIL import Image, ImageFont, ImageDraw
 from bs4 import BeautifulSoup as bs
@@ -65,6 +66,25 @@ class ImageGenerator:
 
 
 class ContestsRetreiver:
+    def getContestDetails(self):
+        # 2020-05-11 15:00:00
+        contestsJson = []
+        contests = []
+        codechefContests = self.getContestsfromCodechef()
+        for contest in codechefContests:
+            if(datetime.now().date == parser.parse(contest['start_time']).date):
+                contestsJson.append(contest)
+        codeforcesContests = self.getContestsfromCodeforces()
+        for contest in codeforcesContests:
+            if(datetime.now().date == parser.parse(contest['start_time']).date):
+                contestsJson.append(contest)
+
+        for json in contestsJson:
+            # self, contestCode, contestName, platform, startDateTime, endDateTime
+            contests.append(Contest(
+                json['code'], json['name'], json['resource'], json['start_time'], json['end_time']))
+        return contests
+
     def getContestsfromCodechef(self):
         URL = "https://www.codechef.com/contests"
         soup = bs(req.get(URL).content, 'lxml')
@@ -79,16 +99,20 @@ class ContestsRetreiver:
             for data in datas:
                 strippedData.append(data.text.strip())
             if(len(strippedData) == 4):
+                startDate = datetime.strptime(
+                    strippedData[2], "%d %b %Y  %H:%M:%S")
+                endDate = datetime.strptime(
+                    strippedData[3], "%d %b %Y  %H:%M:%S")
                 contest['code'] = strippedData[0]
                 contest['name'] = strippedData[1]
-                contest['start_time'] = str(datetime.strptime(
-                    strippedData[2], "%d %b %Y  %H:%M:%S")),
-                contest['end_time'] = str(datetime.strptime(
-                    strippedData[3], "%d %b %Y  %H:%M:%S"))
+                contest['start_time'] = str(startDate)
+                contest['end_time'] = str(endDate)
+                contest['duration'] = str(endDate - startDate)
+                contest['resource'] = "codechef"
                 contests.append(contest)
         return contests
 
-    def getContestfromCodeforces(self):
+    def getContestsfromCodeforces(self):
         URL = "http://codeforces.com/contests"
         soup = bs(req.get(URL).content, 'lxml')
         allTables = soup.find_all('div', attrs={'class': 'datatable'})
@@ -102,10 +126,17 @@ class ContestsRetreiver:
             for data in datas:
                 strippedData.append(data.text.strip())
             if(len(strippedData) == 6):
+                startTime = datetime.strptime(
+                    strippedData[2], "%b/%d/%Y %H:%M")
                 contest['name'] = strippedData[0]
                 contest['code'] = strippedData[0].split()[2]
-                contest['start_time'] = str(datetime.strptime(
-                    strippedData[2], "%b/%d/%Y %H:%M"))
+                contest['start_time'] = str(startTime)
+                if(len(strippedData[3]) == 5):
+                    tempDate = datetime.strptime(strippedData[3], "%H:%M")
+                contest['duration'] = str(timedelta(
+                    hours=tempDate.hour, minutes=tempDate.minute, seconds=tempDate.second))
+                contest['end_time'] = str(startTime + timedelta(
+                    hours=tempDate.hour, minutes=tempDate.minute, seconds=tempDate.second))
                 contest['resource'] = "codeforces"
                 contests.append(contest)
         return contests
