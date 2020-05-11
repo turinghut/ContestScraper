cbc6ddefe9a7efbca411f69d26cfb42bf7d405ac
sindhujaponnuru <sindhuja.ponnuru@gmail.com>
2020-05-11 12:08:10 +0530

    Fixed overflow bug in image generation

--- src/services.py
+++ src/services.py
@@ -68,7 +68,7 @@ class ContestsRetreiver:
         codeforcesContests = self.getContestsfromCodeforces()
         for contest in codeforcesContests:
             if(datetime.now().date() == parser.parse(contest['start_time']).date()):
-                contestsJson.append(contest)
+                contestsJso.append(contest)
 
         return contestsJson
 
